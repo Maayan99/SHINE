5be90050e2e0277ab5a36070e866ded052dd0df4
Yewei-Liu <2300012959@stu.pku.edu.cn>
2025-12-13 12:35:38 +0800

    20251213

--- scripts/Qwen3-8B/pretrain_group_6layer.sh
+++ scripts/Qwen3-8B/pretrain_group_6layer.sh
@@ -10,20 +10,20 @@
 #SBATCH -o metalora.out
 #SBATCH -e metalora.err
 
-NAME=tmp
+NAME=8gpu_4lora_4metalora_lr5e-5_grouppretrain_6layer_1270
 NUM_GPUS=8
-MASTER_PORT=18900             
+MASTER_PORT=18920       
 CONFIG_NAME="Qwen3-8B"       
-SOURCE=transmla
+SOURCE=grouptransmla
 TRAIN_BATCH_SIZE=1
 TEST_BATCH_SIZE=1
 GRADIENT_ACCUMULATION_STEPS=4
 USE_GRADIENT_CHECKPOINT=False
 RESUME_GLOBAL_STEP=latest   # -1: don't resume,   int: resume from global steps,  latest: resume from latest
 LEARNING_RATE=5e-5
+CONVERSATION_MAX_LEN=1270   # Extra base len: 0 Extra chat len per turn: 10
+CONTEXT_MAX_LEN=$((CONVERSATION_MAX_LEN - 10))
 TYPE=transformer
-CONVERSATION_MAX_LEN=1270
-CONTEXT_MAX_LEN=1260
 NUM_LAYERS=6
 WARMUP_STEPS=200
 
@@ -39,6 +39,26 @@ export HYDRA_FULL_ERROR=1
 export OMP_NUM_THREADS=4
 export NCCL_DEBUG=WARN
 export TORCH_DISTRIBUTED_DEBUG=INFO
+
+python generate_group_idx.py  \
+    --config-name $CONFIG_NAME \
+    name=$NAME \
+    mode=pretrain \
+    data.source=$SOURCE \
+    data.train_batch_size=$TRAIN_BATCH_SIZE \
+    data.eval_batch_size=$TEST_BATCH_SIZE \
+    run.gradient_accumulation_steps=$GRADIENT_ACCUMULATION_STEPS \
+    run.use_gradient_checkpoint=$USE_GRADIENT_CHECKPOINT \
+    resume_global_step=$RESUME_GLOBAL_STEP \
+    optim.learning_rate=$LEARNING_RATE \
+    metanetwork.type=$TYPE \
+    data.conversation_max_length=$CONVERSATION_MAX_LEN \
+    data.context_max_length=$CONTEXT_MAX_LEN \
+    metanetwork.transformer_cfg.num_layers=$NUM_LAYERS \
+    optim.warmup_steps=$WARMUP_STEPS \
+    > tmp_pretrain_$NAME.txt 2>&1
+
+wait
 
 nohup torchrun \
     --nproc_per_node=$NUM_GPUS \
@@ -57,9 +77,9 @@ nohup torchrun \
     run.use_gradient_checkpoint=$USE_GRADIENT_CHECKPOINT \
     resume_global_step=$RESUME_GLOBAL_STEP \
     optim.learning_rate=$LEARNING_RATE \
+    metanetwork.type=$TYPE \
     data.conversation_max_length=$CONVERSATION_MAX_LEN \
     data.context_max_length=$CONTEXT_MAX_LEN \
-    metanetwork.type=$TYPE \
     metanetwork.transformer_cfg.num_layers=$NUM_LAYERS \
     optim.warmup_steps=$WARMUP_STEPS \
     > tmp_pretrain_$NAME.txt 2>&1 &
